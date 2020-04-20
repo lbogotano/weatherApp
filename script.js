2bf5f898b424c9a936a01a8b0dd28f560b47bda5
lbogotano <62104715+lbogotano@users.noreply.github.com>
2020-04-20 08:30:49 -0400

    second commit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     
     // $('#submit').click(() =>{
     
-    //     let zip = $('#zipCode').val();
+    //     let zip = $('#input').val();
     //     if (zip != "" ){
         
     
